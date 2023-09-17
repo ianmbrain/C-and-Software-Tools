0ed5c7de0e14a464219f538d52e973172f5148c6
Ian Marshall Brain <imbrain@engr-ras-206.eos.ncsu.edu>
2023-09-17 14:48:37 -0400

    Completed functions in name.c

--- p2/name.c
+++ p2/name.c
@@ -11,23 +11,61 @@
 /** Exit status for a missing or badly formatted name. */
 #define NAME_ERROR 101
 
+/**
+ * Reads in the name from standard input and stores it in the parameter string
+ * Returns false if end of file is reached and there is not text.
+ * Exits with 101 status if the name is longer than 30 characters or does not exist.
+ * @param name Variable to store the name within.
+ * @return False if end of file is reached and there is no name field. Exit with 101 status error if the name is empty or too long. Return true otherwise. 
+*/
 bool read_name( char name[ FIELD_MAX + 1 ] )
 {
-  // Return false if end of file is reached or there is no name field.
-  // MAYBE MAKE 30 FIELD_MAX
-  if( scanf( "%30s", name ) == -1 ) {
+  // NEED TO CHANGE 30 TO FIELD_MAX
+  if ( scanf( "%30s", name ) == -1 ) {
     return false;
   }
 
-  if( strlen( name ) == 0 || strlen( name ) > 30 ) {
+  if ( strlen( name ) == 0 || strlen( name ) > 30 ) {
     exit( NAME_ERROR );
   }
+
+  return true;
 }
 
 void fix_name( char name[ FIELD_MAX + 1 ] )
 {
-  // Exit with status of 101 if the name isn't in an acceptable format.
-  // If the name contains a comma, it is formatted correctly. If the name does not, the last name should be swapped with the first name and a comma added.
-  // This RELIES ON UTIL. I should start at util.c.
+  int length = strlen( name );
+  int comma_count = 0;
 
+  for ( int i = 0; i < length; i++ ) {
+    if ( name[ i ] == ',' )
+      comma_count++;
+    else if ( !isalpha( name[ i ] ) && !isspace( name[ i ] ) )
+      exit( NAME_ERROR );
+  }
+
+  // If the string is formatted with a comma, capitalize the first letter of the first and last name.
+  if ( comma_count == 1 ) {
+    name[ 0 ] = toupper( name[ 0 ] );
+
+    name[ skip_letters( name, 0 ) + 2 ] = toupper( skip_letters( name, 0 ) + 2 );
+  }
+  else if ( comma_count == 0 ) {
+    // DOES THIS ARRAY NEED TO BE RESIZED? NO AS FIELD_MAX + 1 FOR NULL TERMINATOR
+    int space_index = skip_letters( name, 0 );
+
+    char first_name[ FIELD_MAX + 1 ] = "";
+    char last_name[ FIELD_MAX + 1 ] = "";
+
+    strncpy( first_name, name, 0 );
+    strncpy( last_name, name, space_index + 1 );
+
+    strcat( first_name, ', ' );
+    strcat( first_name, last_name );
+    strcat( name, first_name );
+  }
+  else {
+    exit( NAME_ERROR );
+  }
+  // NEED TO EXIT WITH ERROR IF IMPROPER FORMAT
 }
--- p2/util.h
+++ p2/util.h
@@ -3,3 +3,7 @@
 
 /** Maximum length of any field in the input. */
 #define FIELD_MAX 30
+
+int skip_digits( char str[], int start );
+int skip_letters( char str[], int start );
+int copy_substring( char dest[], int dstart, char src[], int sstart, int send );
