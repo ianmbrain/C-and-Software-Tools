60713fe90f1603ef449e41308f11ed3962cd38bb
Ian Marshall Brain <imbrain@engr-ras-203.eos.ncsu.edu>
2023-09-18 09:27:12 -0400

    Completed functions in ssn.c

--- p2/ssn.c
+++ p2/ssn.c
@@ -19,3 +19,63 @@
 
 /** Number of digits in the last group in the SSN. */
 #define END_DIGITS 4
+
+/**
+ * Read in the user inputted ssn to the ssn parameter variable from standard input.
+ * Exit with 103 status if the ssn cannot be read or it is too long.
+ * @param ssn ssn variable to store the user inputted ssn to.
+*/
+void read_ssn( char ssn[ FIELD_MAX + 1 ] ) {
+  // NEED TO CHANGE 11 TO FIELD_MAX
+  if ( scanf( "%11s", ssn ) == -1 ) {
+    exit( SSN_ERROR );
+  }
+
+  if ( strlen( ssn ) != ( START_DIGITS + MIDDLE_DIGITS + END_DIGITS ) || strlen( ssn ) != ( START_DIGITS + MIDDLE_DIGITS + END_DIGITS + 2 ) || strcmp( ssn, "N/A" ) != 0 ) {
+    exit( SSN_ERROR );
+  }
+}
+
+/**
+ * Updates the ssn to the format of three digits, dash, two digits, dash, final four digits.
+ * Exit with 103 status if the ssn is not in one of three formats:
+ * three digits, dash, two digits, dash, final four digits,
+ * nine consecutive digits,
+ * or N/A.
+ * @param ssn ssn to update to the correct format. 
+*/
+void fix_ssn( char ssn[ FIELD_MAX + 1 ] ) {
+  int first_non_digit = skip_digits( ssn, 0 );
+
+  // If the index of a non digit is 0 then the format should be N/A.
+  if ( first_non_digit == 0 ) {
+    if ( strcmp( ssn, "N/A" ) != 0 )
+      exit( SSN_ERROR );
+  }
+  // If the non digit index indicates the ssn is already in standard format.
+  else if ( first_non_digit == 3 ) {
+    if ( ssn[ first_non_digit ] != '-' || ssn[ skip_digits( ssn, first_non_digit + 1 ) ] != '-' )
+      exit( SSN_ERROR );
+  }
+  // Otherwise the ssn should be in the format of just nine numbers. Change this ssn to standard format.
+  else {
+    char cpy_ssn[ FIELD_MAX + 1 ] = "";
+    int cpy_index = 0;
+
+    copy_substring( cpy_ssn, cpy_index, ssn, 0, START_DIGITS );
+    cpy_index += START_DIGITS;
+
+    copy_substring( cpy_ssn, cpy_index, "-", 0, 1 );
+    cpy_index++;
+
+    copy_substring( cpy_ssn, cpy_index, ssn, START_DIGITS, ( START_DIGITS + MIDDLE_DIGITS ) );
+    cpy_index += MIDDLE_DIGITS;
+
+    copy_substring( cpy_ssn, cpy_index, "-", 0, 1 );
+    cpy_index++;
+
+    copy_substring( cpy_ssn, cpy_index, ssn, ( START_DIGITS + MIDDLE_DIGITS ), ( START_DIGITS + MIDDLE_DIGITS + END_DIGITS ) );
+
+    strcpy( ssn, cpy_ssn );
+  }
+}
