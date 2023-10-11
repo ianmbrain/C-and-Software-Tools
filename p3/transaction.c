817e88622019e716d63d4dafb1a3502f27ac2c14
Ian Marshall Brain <imbrain@engr-ras-209.eos.ncsu.edu>
2023-10-11 10:48:24 -0400

    Implemented function for transaction.c

--- p3/transaction.c
+++ p3/transaction.c
@@ -1,6 +1,16 @@
 /**
  */
 
+// I included these, check what libraries we can use __________--_____----_____-------_____----______-------_____----_____------------
+#include "transaction.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+#include "util.h"
+#include "account.h"
+
 // This function reads and performs all transactions in the transaction file with the given name. 
 // It uses the account component to update account balances and detects errors as the transaction file is processed.
 void processTransactons( char const fname[] ) {
@@ -12,5 +22,48 @@ void processTransactons( char const fname[] ) {
 
     // An invalid currency value or a currency value that’s too large should also produce the Invalid transaction file message. You are not expected to check for overflow in reading the number of shares
 
-    
+    char name[ NAME_LIMIT + 1 ];
+    char transaction[ 4 + 1 ];
+    unsigned long shares = 0;
+    double price = 0;
+
+    FILE *file = fopen( fname, "r" );
+
+    while ( fscanf( file, "%s%s%d%f", name, transaction, &shares, &price ) == 1 ) {
+        if ( lookupAccount( name ) == NULL ) {
+            frprintf( stderr, "Invalid transaction file" );
+            exit( EXIT_FAILURE );
+        }
+        
+        fscanf( file, "%s", transaction );
+        if ( strcmp( transaction, "buy" ) != 0 && strcmp( transaction, "sell" ) ) {
+            frprintf( stderr, "Invalid transaction file" );
+            exit( EXIT_FAILURE );
+        }
+        
+        if ( fscanf( file, "%d", &shares ) != 1 ) {
+            frprintf( stderr, "Invalid transaction file" );
+            exit( EXIT_FAILURE );
+        }
+
+        fscanf( file, "%f", &price );
+
+        if ( !checkMul( shares, price ) ) {
+            frprintf( stderr, "Account overflow" );
+            exit( EXIT_FAILURE );
+        }
+
+        unsigned long amount = shares * price;
+        unsigned long *balance = lookupAccount( name );
+        if ( !checkAdd( *balance, amount ) ) {
+            frprintf( stderr, "Account overflow" );
+            exit( EXIT_FAILURE );
+        }
+        
+        *balance += amount;
+    }
+
+    saveAccounts( fname );
+
+    close( file );
 }
--- p3/transaction.h
+++ p3/transaction.h
@@ -1,2 +1,9 @@
 /**
  */
+
+// I added these here from account.c. Check if this is allowed _______-------_________--------_______-------
+/** Maximum length of an account name. */
+#define NAME_LIMIT 30
+
+/** Limit on the length of an account file. */
+#define AFILE_LIMIT 30
