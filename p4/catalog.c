ce47c503aa6ed80704a5adc7840a0794b10c2477
Ian Marshall Brain <imbrain@engr-ras-209.eos.ncsu.edu>
2023-10-22 14:30:42 -0400

    Added starting functions to each file

---
+++ p4/catalog.c
@@ -0,0 +1,28 @@
+
+
+
+
+
+double distance(  Park const *a, Park const *b ) {
+
+}
+
+Catalog *makeCatalog() {
+
+}
+
+void freeCatalog( Catalog *catalog ) {
+
+}
+
+void readParks( char const *filename, Catalog *catalog ) {
+
+}
+
+void sortParks( Catalog *catalog, int (* compare) (void const *va, void const *vb )) {
+
+}
+
+void listParks( Catalog *catalog, bool (*test)( Park const *park, char const *str ), char const *str ) {
+    
+}
---
+++ p4/input.c
@@ -0,0 +1,7 @@
+
+
+#include <stdio.h>
+
+char *readLine( FILE *fp ) {
+
+}
