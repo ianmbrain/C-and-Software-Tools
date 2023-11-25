be8a54ff7a1ea7fbc315b11051d93cd0795d0924
Ian Marshall Brain <imbrain@engr-ras-205.eos.ncsu.edu>
2023-11-25 15:41:34 -0500

    Passing all stringTest tests

--- p6/value.c
+++ p6/value.c
@@ -82,7 +82,9 @@ int parseInteger( Value *v, char const *str )
 
 static void printString( Value const *v )
 {
-  printf( "%s", v->vptr );
+  // Does not include new line character __-_-___--_--___---____--_____--____----____----____-
+  char *return_str = v->vptr;
+  printf( "%s", return_str );
 }
 
 static void moveString( Value const *src, Value *dest )
@@ -100,10 +102,13 @@ static bool equalsString( Value const *v, Value const *other )
 {
   // Make sure the other object is also an Integer.
   // (i.e., it uses the same print funtion)
-  if ( other->print != printInteger )
+  if ( other->print != printString )
     return false;
 
-  if ( strcmp( v, other ) == 0 ) {
+  char *v_string = v->vptr;
+  char *o_string = other->vptr;
+
+  if ( strcmp( v_string, o_string ) == 0 ) {
     return true;
   }
   else {
@@ -117,12 +122,14 @@ static bool equalsString( Value const *v, Value const *other )
 static unsigned int hashString( Value const *v )
 {
   unsigned int hash_value = 0;
-  int size = 0;
-  int str_len = strlen( v->vptr );
+  int size = 1;
+  int str_len = strlen( v->vptr ) - 1;
+  // printf( "\n%d\n", str_len );
   char *str_value = v->vptr;
 
   while ( size < str_len ) {
-    hash_value += ( str_value[ size++ ] - '0' );
+    // printf( "\n%d\n", str_value[ size ] );
+    hash_value += ( str_value[ size++ ] );
     hash_value += hash_value << 10;
     hash_value ^= hash_value >> 6;
   }
@@ -138,15 +145,73 @@ static void emptyString( Value *v )
   free( v->vptr );
 }
 
+// CITE as being based on input.c __-----___---____--_____---____---__-_--____---__-
 int parseString( Value *v, char const *str )
 {
-  int len;
-  char str_value[20] = "";
-  if ( sscanf( str, "%s%n", str_value, &len ) != 1 )
-    return 0;
+  // int len = 0;
+  // char str_value[50] = "";
+  // if ( sscanf( str, "\"%[^\"]\"%n", str_value, &len ) != 1 )
+  //   return 0;
 
-  char *str = ( char * ) malloc( strlen( str_value ) +1 );
-  strcpy( str, str_value );
+  // char *v_str = ( char * ) malloc( strlen( str_value ) +1 );
+  // strcpy( v_str, str_value );
+
+  int len = 0;
+  int num_read = 0;
+  int capacity = 5;
+
+  char *text_string = ( char * ) malloc( capacity * sizeof( char ) );
+  char cur_char = ' ';
+
+  int scan_i = 0;
+  scan_i = sscanf( str, "%c", &cur_char );
+  // num_read++;
+
+  bool first_quote = false;
+  bool second_quote = false;
+
+  while ( true ) {
+    //printf( "\n%c\n", cur_char );
+    if ( scan_i == EOF ) {
+      break;
+    }
+
+    if ( len + 1 >= capacity ) {
+      //printf( "\nHERE4\n");
+      capacity *= 2;
+      text_string = ( char * ) realloc( text_string, capacity * sizeof( char ) );
+    }
+
+    if ( cur_char == '"' && first_quote ) {
+      //printf( "\nHERE3\n");
+      text_string[ len ] = cur_char;
+      text_string[ len + 1 ] = '\0';
+      len++;
+
+      break;
+    }
+
+    if ( cur_char == '"' && !first_quote ) {
+      first_quote = true;
+      //printf( "\nHERE1\n");
+    }
+
+    if ( first_quote && !second_quote ) {
+      //printf( "\nHERE2\n");
+      text_string[ len ] = cur_char;
+      text_string[ len + 1 ] = '\0';
+      len++;
+    }
+    // If the first quote has not been included, increment the number read.
+    else {
+      num_read++;
+    }
+
+    scan_i = 0;
+    // printf( "\n%c\n", cur_char );
+    // printf( "%d\n", num_read );
+    scan_i = sscanf( str + len + num_read, "%c", &cur_char );
+  }
 
   // Fill in all the fields of v for an integer type of value.
   v->print = printString;
@@ -154,8 +219,8 @@ int parseString( Value *v, char const *str )
   v->equals = equalsString;
   v->hash = hashString;
   v->empty = emptyString;
-  v->vptr = str;
+  v->vptr = text_string;
 
   // Return how much of str we parsed.
-  return len;
+  return len + num_read;
 }
